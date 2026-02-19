7c06dda4ac5ca1ca48398f707d287bdce64d1855
Настя <dimova.a.v@mail.ru>
2026-02-19 15:36:22 +0300

    исправлены стили в логине

--- src/pages/LoginFormPage.tsx
+++ src/pages/LoginFormPage.tsx
@@ -55,7 +55,7 @@ const InputWrapper = styled.div<{ error: boolean }>`
     letter-spacing: 0.01em;
     border: 2px solid rgba(0, 0, 0, 0.05);
     border-radius: 14px;
-    12px 16px;
+    padding: 12px 16px;
     margin-bottom: 8px;
   }
 
@@ -63,37 +63,34 @@ const InputWrapper = styled.div<{ error: boolean }>`
     position: absolute;
     left: 16px;
     top: 19px;
-    color: #e6e6e6;
+    color: #A2ACB0;
     background: #fff;
     padding: 0 4px;
     font-size: 16px;
     pointer-events: none;
     transition: 0.2s;
     z-index: 1;
+        top: -10px;
+    left: 8px;
+    font-weight: 600;
+    font-size: 15px;
+    line-height: 147%;
+    letter-spacing: 0.01em;
+    border 2px solid #A2ACB0;
   }
 
   input:focus + label,
   input:not(:placeholder-shown) + label {
     top: -10px;
     left: 8px;
-    color: #007AFF;
     font-weight: 600;
     font-size: 15px;
     line-height: 147%;
     letter-spacing: 0.01em;
   }
- 
-  input:focus {
-    border-color: #007AFF;
-  }
 
-  input:-webkit-autofill + label,
-  input:-moz-autofill + label {
-    top: 6px;
-    transform: none;
-    font-size: 12px;
-    color: #555;
-  }
+
+
 
   input:-webkit-autofill,
   input:-webkit-autofill:hover,
@@ -168,9 +165,9 @@ const SubmitButton = styled.button<{ disabled: boolean }>`
 
 const EyeButton = styled.button`
   position: absolute;
-  right: 16px;
+  right: 10px;
   top: 50%;
-  transform: translateY(-50%);
+  transform: translateY(-60%);
   background: none;
   border: none;
   cursor: pointer;
@@ -182,13 +179,7 @@ const EyeButton = styled.button`
   justify-content: center;
   z-index: 2;
 
-  &:hover {
-    color: #646cff;
-  }
 
-  &:focus {
-    outline: none;
-  }
 `;
 
 const LoginPage: React.FC = () => {
@@ -348,4 +339,4 @@ const LoginPage: React.FC = () => {
   );
 };
 
-export default LoginPage;
+export default LoginPage; 
